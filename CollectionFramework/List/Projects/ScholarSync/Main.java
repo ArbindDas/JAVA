598e3226bbadee2e726f550d1c712eec91f0e660
Arbind Das <dasarbind269@gmail.com>
2025-07-10 22:15:55 +0545

    First project is completed

--- CollectionFramework/List/Projects/ScholarSync/Main.java
+++ CollectionFramework/List/Projects/ScholarSync/Main.java
@@ -2,168 +2,158 @@ package CollectionFramework.List.Projects.ScholarSync;
 
 import java.util.*;
 
-public class Main {
-    public  static  int getIntegerInput(String prompt){
+public class Main{
+    private static final Scanner scanner=new Scanner(System.in);
+    private static final List < Student > students=new ArrayList <>();
+    private static final Map < String, Student > success=new HashMap <>();
+
+    public static int getIntegerInput(String prompt){
         System.out.println(prompt);
         try {
 
-            return Integer.parseInt( sc.nextLine() );
-
-        }catch (NumberFormatException e){
+            return Integer.parseInt(scanner.nextLine());
+        } catch( NumberFormatException e ){
             throw new NumberFormatException(e.getMessage());
         }
     }
 
     public static String getStringInput(String prompt){
         System.out.println(prompt);
-        return sc.nextLine();
-    }
-    private static final Scanner sc = new Scanner( System.in );
-
-    public static final List < Student > studentList = new ArrayList <>();
-
-    private static final Map < String, String > studentMap = new HashMap <>();
-
-    public static void main( String[] args ) {
-
-        while ( true ){
-            Display();
-
-            String Choice = getStringInput( "Enter the choice " );
-
-            switch (Choice.toLowerCase().trim()){
-
-                case "addstudent" -> addstudent();
-
-                case "removestudent" -> removestudent();
-
-                case "viewstudent" -> viewstudent();
-
-                case  "countstudent" -> countstudent();
-
-                case "updatestudent" -> updatestudent();
-
-                case "exit" -> {
-                    try {
-                        Thread.sleep( 10 );
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException( e );
-                    }
-                    System.out.println("Exiting......");
-                    return;
-                }
-            }
-        }
-
-
-
+        return scanner.nextLine();
     }
 
-    private static void updatestudent( ) {
-        Checker( studentList );
-        System.out.println("updating.....");
-
-        int id = getIntegerInput( "Enter the id to be updated...." );
-
-
-
-
-
-
-       Student foundStudent = null;
-
-       for ( Student student : studentList ){
-           if (student.getId()==id){
-               foundStudent = student;
-               break;
-           }
-       }
-
-       if (foundStudent==null){
-           System.out.println("there is no student with id : "+ id);
-       }
-
-        System.out.println("current student info is : "+ foundStudent);
-        System.out.println("now enter the new values ...");
-
-
-
-        String newName = getStringInput( "Enter the new name["+foundStudent.getName() + "]" );
-        if (!newName.isEmpty()){
-            foundStudent.setName( newName );
-        }
-
-        String newAge = getStringInput( "Enter the new age["+foundStudent.getAge()+"]" );
-        if (!newAge.isEmpty()){
-            foundStudent.setAge(Integer.parseInt( newAge ));
-        }
-
-        String newCourse = getStringInput( "Enter the new Course["+foundStudent.getCourse()+"]" );
-        if (!newCourse.isEmpty()){
-            foundStudent.setCourse( newCourse );
-        }
-
-        System.out.println("Student information updated successfully!");
-        System.out.println("updated student is : "+ foundStudent);
-
-    }
-
-    private static void countstudent( ) {
-       long totalStudent = studentList.size();
-        System.out.println( STR."the total student in your list is => \{totalStudent}" );
-    }
-
-    private static void viewstudent( ) {
-        Checker(studentList);
-        System.out.println("the student are ");
-        for ( Student student : studentList ){
-            System.out.println(student);
-        }
-    }
-
-    private static void Checker( List< Student> studentList ) {
+    public static void EmptyCheckerForStudent(List < Student > studentList){
         if (studentList.isEmpty()){
-            System.out.println("there is no student in your list please add first the student and then do this task");
+            System.out.println("student list is empty");
             return;
         }
     }
 
-    private static void removestudent( ) {
-        System.out.println(" Removing student ");
-        Checker( studentList );
-        int id = getIntegerInput( "Enter the student id to be removed" );
-        boolean removed = false;
-        for ( int i = 0; i<studentList.size(); i++ ){
-            if (studentList.get( i ).getId() == id){
-                studentList.remove( i );
-                removed = true;
-                break;
+    public static void main(String[] args){
+
+        while(true){
+            Display();
+            int choice=getIntegerInput("Enter the choice to be performed");
+            switch(choice){
+
+                case 1 -> AddStudent();
+                case 2 -> ViewStudent();
+                case 3 -> SortStudent();
+                case 4 -> UpdateStudent();
+                case 5 -> RemoveStudent();
+                case 6 -> TotalStudent();
+                case 7 -> {
+                    try {
+                        Thread.sleep(1000);
+                    } catch( InterruptedException e ){
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println("Exiting.....");
+                }
             }
         }
-        System.out.println( STR."Successfully student is removed id is \{id}" );
     }
 
-    private static void addstudent( ) {
+    private static void TotalStudent(){
+        long total=students.size();
+        System.out.println(STR."The total student is : \{total}");
+    }
 
-        System.out.println("Add A New Student");
+    private static void RemoveStudent(){
 
-        int id = getIntegerInput( "Enter the Id" );
-        String name = getStringInput( "Enter the Name" );
-        int age = getIntegerInput( "Enter the age" );
-        String course = getStringInput( "Enter the Course" );
+        EmptyCheckerForStudent(students);
+        Integer id=getIntegerInput("Enter the id to be removed");
 
+        boolean removed=false;
+        for(int i=0; i<students.size(); i++){
 
-        studentList.add( new Student(id , name , age , course) );
+            if (students.get(i).getId()==id){
+                students.remove(i);
+                removed=true;
+
+                break;
+            } else{
+                System.out.println(STR."Student is not found with id : \{id}");
+            }
+            if (removed){
+                success.put("the student successfully removed " , students.get(i));
+            } else{
+                System.out.println(STR."Student is not found with id : \{id}");
+            }
+        }
+    }
+
+    private static void UpdateStudent(){
+        EmptyCheckerForStudent(students);
+
+        Integer id=getIntegerInput("Enter the id to be updated");
+
+        Student foundedStudent=null;
+
+        for(int i=0; i<students.size(); i++){
+            if (students.get(i).getId()==id){
+                foundedStudent=students.get(i);
+            }
+        }
+        if (foundedStudent!=null){
+            System.out.println(STR."We are going to update student : \{foundedStudent.getId()}");
+            Integer age=getIntegerInput("Enter the newAge ["+foundedStudent.getAge()+"]:");
+            String name=getStringInput("Enter the new Name["+foundedStudent.getName()+"]");
+            String course=getStringInput("Enter the new Course ["+foundedStudent.getCourse()+"]");
+
+            foundedStudent.setAge(age);
+            foundedStudent.setName(name);
+            foundedStudent.setCourse(course);
+
+            success.put("the student is successfully updated" , foundedStudent);
+
+        } else{
+            System.out.println(STR."there is no student with id : \{id}");
+        }
+
+    }
+
+    private static void SortStudent(){
+        EmptyCheckerForStudent(students);
+        System.out.println("The sorted student are ");
+        List < Student > sortStudent=students.stream()
+                .sorted(Comparator.comparing(Student::getName))
+                .toList();
+        sortStudent.forEach(System.out::println);
+    }
+
+    private static void ViewStudent(){
+
+        EmptyCheckerForStudent(students);
+
+        System.out.println("The students present in your School are");
+        for(Student student : students){
+            System.out.println(student);
+        }
+    }
+
+    private static void AddStudent(){
+
+        System.out.println("Add the Students ");
+
+        Integer id=getIntegerInput("Enter the student id");
+        String name=getStringInput("Enter the student name");
+        Integer age=getIntegerInput("Enter the student age");
+        String course=getStringInput("Enter the course name");
+
+        Student student=new Student(id , name , age , course);
+        students.add(student);
+        success.put("student added successfully " , student);
     }
 
     public static void Display(){
-        System.out.println("------------- The ScholarSync Project --------------");
-        System.out.println("addstudent");
-        System.out.println("removestudent");
-        System.out.println("viewstudent");
-        System.out.println("coutstudent");
-        System.out.println("updatestudent");
-        System.out.println("5.Sort Student");
-        System.out.println("6.Exit");
+        System.out.println("--------- Welcome to my ScholarSync ---------");
+        System.out.println("1.AddStudent");
+        System.out.println("2.ViewStudent");
+        System.out.println("3.SortStudent");
+        System.out.println("4.UpdateStudent");
+        System.out.println("5.RemoveStudent");
+        System.out.println("6.TotalStudent");
+        System.out.println("7.Exit");
     }
 }
--- CollectionFramework/List/Projects/ScholarSync/Student.java
+++ CollectionFramework/List/Projects/ScholarSync/Student.java
@@ -2,49 +2,46 @@ package CollectionFramework.List.Projects.ScholarSync;
 
 public class Student {
 
-    private int id;
+    private Integer id;
     private String name;
-    private int age;
+    private Integer age;
     private String course;
 
-
-
-    public Student(){}
-
-    public Student( int id , String name , int age , String course ) {
+    public Student( Integer id , String name , Integer age , String course ) {
         this.id = id;
-        this.age= age;
         this.name = name;
+        this.age = age;
         this.course = course;
     }
 
+    public Student(){}
 
-    public int getId( ) {
+    public Integer getId( ) {
         return id;
+    }
+
+    public void setId( Integer id ) {
+        this.id = id;
     }
 
     public String getName( ) {
         return name;
     }
 
-    public int getAge( ) {
-        return age;
-    }
-
-    public String getCourse( ) {
-        return course;
-    }
-
-    public void setId( int id ) {
-        this.id = id;
-    }
-
     public void setName( String name ) {
         this.name = name;
     }
 
-    public void setAge( int age ) {
+    public Integer getAge( ) {
+        return age;
+    }
+
+    public void setAge( Integer age ) {
         this.age = age;
+    }
+
+    public String getCourse( ) {
+        return course;
     }
 
     public void setCourse( String course ) {
@@ -53,7 +50,13 @@ public class Student {
 
     @Override
     public String toString() {
-        return STR."ID: \{id}, Name: \{name}, Age: \{age}, Course: \{course}";
+        return STR."""
+            ---------------------------
+            ID     : \{id}
+            Name   : \{name}
+            Age    : \{age}
+            Course : \{course}
+            """;
     }
 
 }
